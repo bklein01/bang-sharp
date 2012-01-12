97894ec15b1039f515393e6e04c09f06401c1016
omosnacek@gmail.com <omosnacek@gmail.com@f1b0636a-6c76-2858-2483-03a3c2551168>
2012-01-12 19:03:35 +0000

    * AssemblyInfo.cs:
    * GameCycle.cs:
      Fixed the predraw check priority.

--- trunk/Bang# Server Daemon/Session/Game/GameCycle.cs
+++ trunk/Bang# Server Daemon/Session/Game/GameCycle.cs
@@ -150,7 +150,7 @@ namespace Bang.Server
 			List<TableCard> table = new List<TableCard>(RequestedPlayer.Table);
 			// the list must be copied, or any removal from the table
 			// causes the enumerator to throw an InvalidOperationException
-			table.Sort((a, b) => b.PredrawCheckPriority - a.PredrawCheckPriority);
+			table.Sort((a, b) => a.PredrawCheckPriority - b.PredrawCheckPriority);
 			foreach(TableCard card in table)
 				try
 				{
