05c1768934bfb7b3e291ea4508d0360dcc80f447
omosnacek@gmail.com <omosnacek@gmail.com@f1b0636a-6c76-2858-2483-03a3c2551168>
2012-06-17 17:00:30 +0000

    * AssemblyInfo.cs:   Updated the assembly version.
    * DocHolyday.cs:
      Removed meaningless condition for target player in Doc Holyday.
    
    * Dodge.cs:
      Draw card before reporting missed to the result callback in Dodge so
      that Suzy Lafayette doesn't draw twice after playing Dodge.

--- trunk/Bang# Server Daemon/Session/Game/Cards/Dodge City/Dodge.cs
+++ trunk/Bang# Server Daemon/Session/Game/Cards/Dodge City/Dodge.cs
@@ -35,8 +35,8 @@ namespace BangSharp.Server.Daemon.Cards
 		public override void CheckMissed(CardResultCallback resultCallback)
 		{
 			Player owner = Owner;
-			resultCallback(this, true);
 			Game.GameTable.PlayerDrawFromDeck(owner, 1);
+			resultCallback(this, true);
 		}
 	}
 }
