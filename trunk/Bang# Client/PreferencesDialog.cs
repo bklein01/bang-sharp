970e9045c935ef94acbcd0ead688820d49b585d8
omosnacek@gmail.com <omosnacek@gmail.com@f1b0636a-6c76-2858-2483-03a3c2551168>
2012-09-22 09:15:57 +0000

    * gui.stetic: * PreferencesDialog.cs: * BangSharp.Client.PreferencesDialog.cs:   Added request timeout to the preferences dialog.
    * sk.po:
    * cs.po:
    * en.po:
    * messages.po:
      Updated translations.
    
    * AssemblyInfo.cs:
      Updated the assembly version.

--- trunk/Bang# Client/PreferencesDialog.cs
+++ trunk/Bang# Client/PreferencesDialog.cs
@@ -39,10 +39,12 @@ namespace BangSharp.Client
 			autoHideSWWhilePlayingCheckbox.TooltipMarkup = Catalog.GetString("If checked, the Session Window automatically hides when the game starts and shows when the game ends.");
 			animDelayEntry.TooltipMarkup = Catalog.GetString("Minimum time between rendering of frames of the game board. Decreasing this value may get you higher FPS, but it may also increase the CPU utilisation.");
 			animDurationEntry.TooltipMarkup = Catalog.GetString("The default duration of animation (card movement, etc).");
+			requestTimeoutEntry.TooltipMarkup = Catalog.GetString("The amount of time to wait for the server to return a response.");
 
 			autoHideSWWhilePlayingCheckbox.Active = Config.Instance.GetBoolean("Client.AutoHideSWWhilePlaying", true);
 			animDelayEntry.Value = Config.Instance.GetInteger("Client.AnimDelay", 100);
 			animDurationEntry.Value = Config.Instance.GetInteger("Client.DefaultAnimDuration", 1000);
+			requestTimeoutEntry.Value = Config.Instance.GetInteger("Client.RequestTimeout", 30000);
 		}
 
 		protected void OnResponse(object o, Gtk.ResponseArgs args)
@@ -52,6 +54,7 @@ namespace BangSharp.Client
 				Config.Instance.SetBoolean("Client.AutoHideSWWhilePlaying", autoHideSWWhilePlayingCheckbox.Active);
 				Config.Instance.SetInteger("Client.AnimDelay", animDelayEntry.ValueAsInt);
 				Config.Instance.SetInteger("Client.DefaultAnimDuration", animDurationEntry.ValueAsInt);
+				Config.Instance.SetInteger("Client.RequestTimeout", requestTimeoutEntry.ValueAsInt);
 			}
 			if(args.ResponseId == Gtk.ResponseType.Ok || args.ResponseId == Gtk.ResponseType.Cancel || args.ResponseId == Gtk.ResponseType.DeleteEvent)
 				Destroy();
